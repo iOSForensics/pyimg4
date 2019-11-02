69bc95e7142697ea76d2faff5fbb6ed2e3fc890f
Mathieu Renard <mathieu.renard@gotohack.org>
2019-11-02 20:21:15 +0100

    Add Licence

--- pyimg4/img4.py
+++ pyimg4/img4.py
@@ -1,4 +1,25 @@
-import sys
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+#
+#  pyimg4 A python lib for manipulating IMG4, IM4M and IM4P files.
+#
+# Copyright (c) 2017 "dark[-at-]gotohack.org"
+#
+#
+# pymobiledevice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#import sys
 from scapy.asn1packet import *
 from scapy.asn1fields import *
 from scapy.layers.x509 import *
