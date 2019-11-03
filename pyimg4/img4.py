acfcedcabf9082e1b95c30bff1817bbaab0f626a
Mathieu Renard <GotoHack@users.noreply.github.com>
2019-11-03 12:30:18 +0100

    Fix typo

--- pyimg4/img4.py
+++ pyimg4/img4.py
@@ -19,7 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#import sys
+
+import sys
+
 from scapy.asn1packet import *
 from scapy.asn1fields import *
 from scapy.layers.x509 import *
